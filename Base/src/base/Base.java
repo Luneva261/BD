22138485f63ba397914e4de191a9bbc02de2d106
Надежда Лунева < >
2016-12-15 23:32:35 +0300

    подключила БД

--- Base/src/base/Base.java
+++ Base/src/base/Base.java
@@ -8,11 +8,12 @@ package base;
  *
  * @author windows
  */
+
+
 public class Base {
 
-    /**
-     * @param args the command line arguments
-     */
+    String driver = "org.apache.derby.jdbc.ClientDriver";
+    String url = "jdbc:derby://localhost:1527/sample;user=app;password=app;create=false";
     public static void main(String[] args) {
         // TODO code application logic here
     }
